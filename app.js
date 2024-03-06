f6c6f61fccaa69d471ffc8b6dded52aa7bea62fa
joel thomas mathew <joelmathew857@gmail.com>
2024-03-06 19:37:40 +0530

    ass

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const events = require("events");
+const path = require("path");
+const fs = require("fs");
+const Myemitter = new events();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
-app.get("/:submit", (req, res) => {
-  const finds = req.params.submit;
-  console.log(finds);
-  res.send(`you enter is ${finds}`);
-});
-
-app.get("/sum", (req, res) => {
-  // Destructure the 'a' and 'b' query parameters from req.query
-  const { a, b } = req.query;
-
-  // Parse the query parameters to integers and calculate the sum
-  const result = parseInt(a) + parseInt(b);
-
-  // Send the result back as a response
-  res.send(`the sum is ${result}`);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(3002, () => {
+  console.log(`The server is listening on port ${3002}`);
 });
